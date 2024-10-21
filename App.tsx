a4b6bae9f67868198119da130b18254925661c38
tatoon0 <qudwjd4296@naver.com>
2024-10-21 18:36:19 +0900

    add barcode test button

--- App.tsx
+++ App.tsx
@@ -1,29 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import { productInfo, productData } from './data';
 
 const App = () => {
   const [productInfo, setProductInfo] = React.useState<productInfo | null>(null);
 
+  const handleBarCodeRead = (barcode : string) => {
+    if (productData[barcode]) {
+      setProductInfo(productData[barcode]);
+    } else {
+      setProductInfo(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <RNCamera
         onBarCodeRead={(barcode) => {
           const scannedBarcode = barcode.data;
-
-          if (productData[scannedBarcode]) {
-            setProductInfo(productData[scannedBarcode]);
-          } else {
-            setProductInfo(null);
-          }
-
+          handleBarCodeRead(scannedBarcode);
           console.log('Barcode:', scannedBarcode);
         }}
         style={{ flex: 1 }}
         type={RNCamera.Constants.Type.back}
         autoFocus={RNCamera.Constants.AutoFocus.on}
       />
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        paddingVertical: 10,
+        }}>
+        <Button
+          title="test1"
+          onPress={() => {
+            handleBarCodeRead('2221234567895');
+            console.log(productInfo); 
+          }}
+        />
+        <Button
+          title="test2"
+          onPress={() => {
+            handleBarCodeRead('0036000291452');
+            console.log(productInfo); 
+          }}
+        />
+      </View>
       <Text style={{ 
         textAlign: 'center',
         fontSize: 30, 
